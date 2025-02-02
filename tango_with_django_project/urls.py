fa4546f37964700ec7398fc6a80eb1a297185f68
James Glover <3057283g@student.gla.ac.uk>
2025-02-02 12:06:11 +0000

    Added rango project and .gitignore file

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("", views.index, name="index"),
+    path("rango/", include("rango.urls")),
+    # The above maps any URLs starting with rango/ to be handled by rango
+    path("admin/", admin.site.urls),
+    path("hello/", include("hello.urls")),
+    path("newyear/", include("newyear.urls")),
+    path("tasks/", include("tasks.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
